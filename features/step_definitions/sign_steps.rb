fc4682d1c1f453b0fae352524d768a1a7570e26b
Maxim <parallel588@gmail.com>
2010-02-01 14:13:03 +0500

    Добавил фьютерсы входа и выхода из сервиса

---
+++ features/step_definitions/sign_steps.rb
@@ -0,0 +1,27 @@
+Допустим /^в сервисе зарегистрированы следующие пользователи:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Если /^Я перешел на страницу входа$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+То /^Я должен быть переправлен на страницу "([^\"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Допустим /^в сервисе нет зарегистрированных пользователей$/ do
+  pending # express the regexp above with the code you wish you had
+end
+Допустим /^Я зашел в сервис как "([^\"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+То /^должен быть переправлен на страницу "([^\"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+Допустим /^у нас есть следующие пользователи$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
