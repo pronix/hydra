21ff443bef09c9bfa5ef248939e603333cf7228f
Maxim <parallel588@gmail.com>
2010-02-01 22:00:17 +0500

    Фьючерс по account

---
+++ features/step_definitions/account_steps.rb
@@ -0,0 +1,39 @@
+Допустим /^Я зашел как пользователь "([^\"]*)"  с паролем "([^\"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Допустим /^перешел на страницу учетной записи$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+То /^должен увидеть свой "([^\"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Допустим /^нахожуь на странице учетной записи$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Если /^Я перешел по ссылке "([^\"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+То /^Я должен увидеть форму редактирования учетной записи$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+То /^должен увидеть кнопку "([^\"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Допустим /^Я зашел как пользователь "([^\"]*)" с паролем "([^\"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Допустим /^нахожусь странице редактирования учетной записи$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+То /^должен увидеть свой новый "([^\"]*)" равный "([^\"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
