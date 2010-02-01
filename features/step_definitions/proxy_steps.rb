8ad1a9425d14d2a99d17bf05781e66bcce657bc5
Maxim <parallel588@gmail.com>
2010-02-01 21:45:51 +0500

    Фьючерс по proxy

---
+++ features/step_definitions/proxy_steps.rb
@@ -0,0 +1,50 @@
+Допустим /^у меня есть список proxy:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Допустим /^Я перешел на страницу Proxy$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+То /^должен увидеть список proxy:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Допустим /^Я на странице редактирования proxy$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Если /^Я изменил поле "([^\"]*)" с "([^\"]*)" на "([^\"]*)"$/ do |arg1, arg2, arg3|
+  pending # express the regexp above with the code you wish you had
+end
+
+То /^должен увидеть proxy c "([^\"]*)" равный "([^\"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Если /^Я удаляю proxy "([^\"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+То /^Я должен увидеть оставшийся список proxy:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Допустим /^Я на странице добавление Proxy$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Допустим /^у меня пока нет proxy$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Если /^нажал кноку "([^\"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+То /^в список proxy не должен быть пустым$/ do
+  pending # express the regexp above with the code you wish you had
+end
