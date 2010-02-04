0f7d4ffa7cbf6bedd232b86aaf1032b75252cb17
Maxim <parallel588@gmail.com>
2010-02-04 16:44:09 +0500

    user session

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,25 @@
+class UserSessionsController < ApplicationController
+  before_filter :require_no_user, :only => [:new]
+  before_filter :require_user, :only => :destroy
+  
+  def new
+    
+    if request.post?
+      @user_session = UserSession.new(params[:user_session])
+      if @user_session.save
+        flash[:notice] = I18n.t("login_successful")
+        redirect_back_or_default root_path
+      else
+        render :action => :new
+      end
+    else
+      @user_session = UserSession.new
+    end
+    
+  end
+
+  def destroy
+    current_user_session.destroy
+    redirect_back_or_default root_path
+  end
+end
