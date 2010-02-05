f7050ff5ad571f655c8fa411de4f2fefaa2df64c
Maxim <parallel588@gmail.com>
2010-02-05 19:19:32 +0500

    изменил поле авторизации

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
-  acts_as_authentic 
+  acts_as_authentic  do |c|
+    c.login_field = 'email'
+  end
+  validates_presence_of :notification_email
+  
+  before_validation :set_notification_email
+  def set_notification_email
+    if self.notification_email.blank?
+      self.notification_email = self.email
+    end
+  end
 end
