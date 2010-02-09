7fb3c27a82e74457735d9a60543c56f1ce27a75b
Maxim <parallel588@gmail.com>
2010-02-09 12:35:34 +0500

    редактирование макросов

--- app/controllers/macros_controller.rb
+++ app/controllers/macros_controller.rb
@@ -1,6 +1,18 @@
 class MacrosController < ApplicationController
   inherit_resources 
   defaults :resource_class => Macros, :collection_name => 'macros', :instance_name => 'macro'
+ 
+  def create
+    create! do |success, failure|
+      success.html { redirect_to categories_path }
+    end
+  end
+  
+  def update
+    update! do |success, failure|
+      success.html { redirect_to categories_path }
+    end
+  end
   
   protected
   def begin_of_association_chain
