fe86e1243412ead9c277ac28623e25aefd391e36
Maxim <parallel588@gmail.com>
2010-02-10 00:39:45 +0500

    сделал в редактирование категории в всплывающем окне

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,14 +3,12 @@ class CategoriesController < ApplicationController
   defaults :resource_class => Category,
            :collection_name => 'categories', :instance_name => 'category'
 
-  respond_to :html
-  respond_to :js,  :layout => false
+  respond_to :html, :js
 
   def edit
-    @category = Category.find params[:id]
-    respond_to do |format|
-      format.html{ render :edit}
-      format.js { render :edit, :layout => false  }
+    edit! do |format|
+      format.html { render :action => :edit }
+      format.js   { render :action => :edit, :layout => false }
     end
   end
 
