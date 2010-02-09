1d79328dca9789632ab0f2639d2d359aa668475e
Maxim <parallel588@gmail.com>
2010-02-09 13:52:35 +0500

    Первый вариант всплывающих слоев для диалогов

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,19 +1,31 @@
 class CategoriesController < ApplicationController
   inherit_resources
-  defaults :resource_class => Category, 
+  defaults :resource_class => Category,
            :collection_name => 'categories', :instance_name => 'category'
+
+  respond_to :html
+  respond_to :js,  :layout => false
+
+  def edit
+    @category = Category.find params[:id]
+    respond_to do |format|
+      format.html{ render :edit}
+      format.js { render :edit, :layout => false  }
+    end
+  end
 
   def create
     create! do |success, failure|
       success.html { redirect_to categories_path }
     end
   end
-  
+
+
   def update
     update! do |success, failure|
       success.html { redirect_to categories_path }
     end
   end
-  
+
 end
 
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,49 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function(){
+
+function js_link_to_content(url,el) {
+    var dialog = $(el).parents("div.ui-dialog-content");
+    var _url = url.split('?')
+        _url = [[_url[0],'js'].join('.'),_url[1]].join('?')
+        $(dialog).load(_url);
+    return false;
+
+
+  };
+
+
+function js_link(url,dialog){
+    $(dialog).load(url+'.js', function(data) {
+
+        var options = { resizable: false,    modal: true, zIndex: 3000,
+                        close: function(event, ui) { $(dialog).remove(); },
+                        width: $(window).width()-100,
+                        height: $(window).height()-100    };
+
+        if ($(data).find("form").length == 0) {
+          options.buttons =  { "Refresh": function() {  js_link(url,dialog);   } };
+        };
+
+        $(dialog).dialog(options);
+      });
+    return false;
+
+
+  };
+
+
+   $(document).click(function(e){
+      var el = $(e.target);
+
+      if ($(el).is("a") && $(el).hasClass("js_link")){
+         var dialog = $("<div></div>").insertAfter('.content');
+         js_link($(el).attr('href'), dialog);
+         return false;
+      } else if ($(el).is("a") && $(el).hasClass("js_link_to_content") ) {
+         js_link_to_content($(el).attr('href'),el);
+         return false;
+      } else {
+         return true; };
+   });
+
+});
+
