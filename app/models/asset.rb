dccf35b596aae2ffcdbda8b7a902c971e02a64e9
Maxim <parallel588@gmail.com>
2010-02-12 16:32:38 +0500

    fix

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,9 +1,6 @@
 class Asset < ActiveRecord::Base
   belongs_to :assetable, :polymorphic => true
 
-  validates_presence_of :title
-
-  # TODO нужно  добавить индекс на это поле
   named_scope :images, :conditions => [%{ attachment_content_type like ? },"image%" ]
 
   def image?
