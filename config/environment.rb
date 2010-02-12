f090cc5fe4e29f44d94b685f2ee39441e43891b4
Maxim <parallel588@gmail.com>
2010-02-12 19:15:16 +0500

    inherited_resources подключил как гем

--- config/environment.rb
+++ config/environment.rb
@@ -21,8 +21,8 @@ Rails::Initializer.run do |config|
   config.gem "acl9", :source => "http://gemcutter.org", :lib => "acl9"
   config.gem "geoip"
   config.gem 'paperclip', :source => 'http://gemcutter.org'
-  
-  
+  config.gem "inherited_resources", :version => '=1.0.3'
+
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
