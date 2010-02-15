5169f8aea119ebcfac315e5677f6d79a85866abc
Maxim <parallel588@gmail.com>
2010-02-15 17:42:45 +0500

    Добавил демон который проверяет зугружены ли файлы или нет

---
+++ script/monitor_downloading.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'environment'))
+require 'optparse'
+require "daemon-spawn"
+
+SLEEP = 5
+class DaemonMonitorDownloading < DaemonSpawn::Base
+  def start(args)
+    loop do
+      begin
+        DownloadingFile.process
+      rescue
+        STDERR.puts "Monitor Downloading start :error"
+      end
+      sleep(SLEEP)
+    end
+  end
+
+  def stop
+    puts "Monitor Downloading stopping"
+  end
+end
+ # DownloadingFile.process
+DaemonMonitorDownloading.spawn!(:working_dir => File.join(File.dirname(__FILE__)),
+                   :log_file => "#{RAILS_ROOT}/log/monitor_downloading.log",
+                   :pid_file => "#{RAILS_ROOT}/tmp/pids/monitor_downloading.pid",
+                   :sync_log => true,
+                   :singleton => true)
