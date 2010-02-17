fa678bb73067f01d8cc7444bc6a56ac3e78edaab
Maxim <parallel588@gmail.com>
2010-02-17 21:25:00 +0500

    Добавил валидацию на позицию временного штампа и формата файла

--- app/models/macros.rb
+++ app/models/macros.rb
@@ -1,3 +1,9 @@
 class Macros < ActiveRecord::Base
   belongs_to :logo, :class_name => UserFile
+  validates_inclusion_of :font, :in => Common::Font.valid_options
+  validates_inclusion_of :position_timestamp,
+                         :in => Common::PositionTimestamp.valid_options,
+                         :if => lambda{ |t| t.add_timestamp? }
+  validates_inclusion_of :file_format, :in => Common::FileFormat.valid_options
+
 end
