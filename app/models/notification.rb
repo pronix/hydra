a2a59346841e13798bf8d9ae4f7a1addae645127
Maxim <parallel588@gmail.com>
2010-02-25 00:42:09 +0500

    fix отправка уведомлений

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -7,7 +7,7 @@ class Notification < ActionMailer::Base
     from        ActionMailer::Base.smtp_settings[:user_name]
     recipients  task.user.email
     sent_on     Time.now
-    body        :task => task
+    body        :task_url => task_url(task), :task_name => task.name
   end
 
   # Уведомление о завершение задачи с ошибкой
@@ -16,7 +16,8 @@ class Notification < ActionMailer::Base
     from        ActionMailer::Base.smtp_settings[:user_name]
     recipients  task.user.email
     sent_on     Time.now
-    body        :task => task
+    body        :task_url => task_url(task),
+    :task_error => (task.job_loggings.last(:conditions => ["job not in ('error')"]).comment rescue "")
   end
 
 end
