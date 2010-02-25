d4d8a480c43ded5e378f1f9df9db7912c012c5cf
Maxim <parallel588@gmail.com>
2010-02-25 18:51:19 +0500

    изменены даннеы по умолчанию

--- db/sample/user.rb
+++ db/sample/user.rb
@@ -3,6 +3,4 @@
   User.all.each { |u| u.has_role! user_role.name.to_sym }
  end
 
- admin_role = Role.find_or_create_by_name "admin"
- admin_user = User.find_by_email "hydra@gmail.com"
- admin_user.has_role! admin_role.name.to_sym
+
