1f128e9a3de993b01f83d2a05842f835526d4463
Maxim <parallel588@gmail.com>
2010-02-25 18:51:58 +0500

    Добавил всплывающие подсказки

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,6 @@
 $(function(){
 
+
 function js_link_to_content(url,el) {
     var dialog = $(el).parents("div.ui-dialog-content");
     var _url = url.split('?')
@@ -11,17 +12,17 @@ function js_link_to_content(url,el) {
   };
 
 
-function js_link(url,dialog){
+function js_link(url,dialog, _height, _width){
     $(dialog).load(url+'.js', function(data) {
 
         var options = { resizable: false,    modal: true, zIndex: 3000,
                         close: function(event, ui) { $(dialog).remove(); },
-                        width: $(window).width()-100,
-                        height: $(window).height()-100    };
+                        width: _width,
+                        height: _height };
 
-        if ($(data).find("form").length == 0) {
+        /* if ($(data).find("form").length == 0) {
           options.buttons =  { "Refresh": function() {  js_link(url,dialog);   } };
-        };
+          };*/
 
         $(dialog).dialog(options);
       });
@@ -31,13 +32,22 @@ function js_link(url,dialog){
   };
 
 
+
    $(document).click(function(e){
       var el = $(e.target);
-
+      var hw;
       if ($(el).is("a") && $(el).hasClass("js_link")){
          var dialog = $("<div></div>").insertAfter('.content');
-         js_link($(el).attr('href'), dialog);
+
+         if ($(el).attr("data_size")) { hw = $(el).attr("data_size").split("x"); };
+         var _height = $(window).height()-100;
+         var _width  = $(window).width()-100;
+         if (hw && hw[0]) {_height = hw[0] };
+         if (hw && hw[1]) {_width = hw[1] };
+
+         js_link($(el).attr('href'), dialog, _height, _width);
          return false;
+
       } else if ($(el).is("a") && $(el).hasClass("js_link_to_content") ) {
          js_link_to_content($(el).attr('href'),el);
          return false;
